b40f2865c6ac1e73139641f544c2d83f05941cff
hogarehalli srinivas <srinivas.ramesha@gmail.com>
2018-05-19 16:44:42 +0000

    instanciated model and connected

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -10,9 +10,26 @@ sap.ui.define([
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 		 * @memberOf view.main
 		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+			var oModel = new sap.ui.model.json.JSONModel();
+			oModel.loadData("localData.json");
+			sap.ui.getCore().setModel(oModel);
+		},
+		onPress: function() {
+			var oMod = this.getView().getModel();
+			if (oMod.getProperty("/empDetails/enable") === false) {
+				oMod.setProperty("/empDetails/enable", true);
+			} else {
+				oMod.setProperty("/empDetails/enable", false);
+			}
+
+		},
+		onChange: function() {
+			var oMode = sap.ui.getCore().getModel();
+			var oP = oMode.getProperty("/empDetails/empName");
+			var oTxt = this.getView().byId("idTxt");
+			oTxt.setText(oP);
+		}
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
